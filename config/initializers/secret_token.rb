c1fb33e7a612478802d442df3913ced3953d24f1
Gabriel Reis and Vic Ramon <V2tkV01sRkhhR2hqTW1oNVlqSk9jbHBZVVhWWk1qbDA=+greis+vicramon@V2tkV01sRkhhR2hqTW1oNVlqSk9jbHBZVVhWWk1qbDA=>
2013-06-21 14:24:28 -0400

    New app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VimGenius::Application.config.secret_key_base = 'b087671858edbec5e6de2d59720090d43fab09dc9f8036a5ef79dec3ef7757bf119e314c2705cf9ca3fb7dcbbc6522e7fa2d452b40c14325dd738232ed19a96e'
