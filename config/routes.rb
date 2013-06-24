00e696d65620c52ee701dbf591c91606bc12299c
Vic Ramon <dev+vicramon@hashrocket.com>
2013-06-24 16:41:18 -0400

    Create page to select skill level

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,12 @@ VimGenius::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
 
   root to: 'home#index'
-
   resources :admin, only: [:index]
-
   resources :lessons
 
-  match '/admin/login', to: 'admin#login', as: 'admin_login', via: [:get, :post]
+  match 'admin/login', to: 'admin#login', as: 'admin_login', via: [:get, :post]
+
+  get 'start', to: 'home#start', as: 'start'
 
   namespace 'admin' do
     root to: 'admin#index'
