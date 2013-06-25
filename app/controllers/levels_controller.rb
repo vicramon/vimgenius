ff3706e71503e4a2a289791af7a09975c2ee8afc
Vic Ramon <dev+vicramon@hashrocket.com>
2013-06-25 14:33:58 -0400

    Show correct command name and level

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -2,6 +2,10 @@ class LevelsController < ApplicationController
   expose(:lesson) { Lesson.find_by_slug(params[:lesson_id]) }
   expose(:levels) { lesson.levels }
   expose(:level, attributes: :level_params, finder: :find_by_slug)
+  expose(:commands) { level.commands }
 
+  def show
+
+  end
 
 end
