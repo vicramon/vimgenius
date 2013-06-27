5d86d60570331d81bdd3b5e4f81945e102d4145a
Vic Ramon <dev+vicramon@hashrocket.com>
2013-06-27 14:26:49 -0400

    Switch from mousetrap to checking text in div

--- app/assets/javascripts/command.js
+++ app/assets/javascripts/command.js
@@ -1,16 +1,59 @@
 
-var keystrokes = $('.success').attr("data-attribute-keystroke").split("or");
+var keystrokes = $.map($('.success').attr("data-attribute-keystroke").split("or"), $.trim);
+var keys_pressed_div = $('#keys_pressed');
+var user_text = '';
+var mousetrap_list = ["escape", "ctrl"]
 
-$.each(keystrokes, function(index, keystroke ) {
-  Mousetrap.bind($.trim(keystroke), function() { success_command(); return false; });
+apply_mousetrap_if_needed();
+
+$(document).keyup( function() {
+  user_text = keys_pressed();
+  $.each(keystrokes, function (index, keystroke) {
+    if (user_text == keystroke) { success_command(); }
+  });
 });
+
+function apply_mousetrap_if_needed() {
+  if (mousetrap_required()) { bind_mousetrap(); }
+}
+
+function contains(string, look_up) {
+  string = string.join();
+  var contains_special_key = false;
+  $.each(look_up, function(index, look_up_item) {
+    if (string.indexOf(look_up_item) != -1) { contains_special_key = true; }
+  });
+  return contains_special_key;
+}
+
+function mousetrap_required() {
+  return contains(keystrokes, mousetrap_list)
+}
+
+function bind_mousetrap() {
+  $.each(keystrokes, function(index, keystroke) {
+    Mousetrap.bind(keystroke, function() { success_command(); });
+  });
+}
+
+function unbind_mousetrap() {
+  $.each(keystrokes, function(index, keystroke) {
+    Mousetrap.unbind(keystroke);
+  });
+}
+
+function keys_pressed() {
+  return keys_pressed_div.text();
+}
 
 $('a.next').click( function() { next_command(); return false; });
 
 function success_command() {
+  capture_keypress = false;
   $('.success').show();
   $('.error').hide();
   stop_timer();
+  unbind_mousetrap();
   Mousetrap.bind("enter", function() { next_command(); return false; });
 }
 
@@ -18,14 +61,8 @@ function next_command() {
   reset_timer();
   start_timer();
   unbind_enter();
-  unbind_keystrokes();
   replace_question();
-}
-
-function unbind_keystrokes() {
-  $.each(keystrokes, function(index, keystroke ) {
-    Mousetrap.unbind(keystroke);
-  });
+  capture_keypress = true;
 }
 
 function unbind_enter() {
--- app/assets/javascripts/keys_pressed.js
+++ app/assets/javascripts/keys_pressed.js
@@ -1,23 +1,26 @@
 
-var keys_pressed = $('#keys_pressed');
+var keys_pressed_div = $('#keys_pressed');
 var correct_keystroke = $('.success').attr("data-attribute-keystroke").replace(/\s+/g, '');
+var capture_keypress = true
 
 Mousetrap.bind("escape", function() { reset_keys_pressed(); return false; });
 Mousetrap.bind("backspace", function() { delete_last_key_press(); return false; });
 
 document.onkeypress = function(event) {
-  display_keypress(event)
+  if (capture_keypress) {
+    display_keypress(event)
+  }
 }
 
 function display_keypress(event) {
   event = event || window.event;
   var character = String.fromCharCode(event.keyCode);
-  keys_pressed.append(character);
+  keys_pressed_div.append(character);
   reset_if_too_long();
 }
 
 function reset_if_too_long() {
-  var key_length = $.trim(keys_pressed.text()).length;
+  var key_length = $.trim(keys_pressed_div.text()).length;
   if (key_length > correct_keystroke.length + 2) {
     reset_keys_pressed();
     display_error();
@@ -25,12 +28,12 @@ function reset_if_too_long() {
 }
 
 function reset_keys_pressed() {
-  keys_pressed.text('');
+  keys_pressed_div.text('');
 }
 
 function delete_last_key_press() {
-  var current_text = keys_pressed.text();
-  keys_pressed.text(current_text.slice(0,-1));
+  var current_text = keys_pressed_div.text();
+  keys_pressed_div.text(current_text.slice(0,-1));
 }
 
 function display_error() {
