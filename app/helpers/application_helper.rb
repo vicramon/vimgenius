1777c5733d7f2f7aaf97783fc04adc50e8506e75
Vic Ramon <dev+vicramon@hashrocket.com>
2013-06-27 14:27:36 -0400

    Fix bug in active selector for list of levels

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,10 @@
 module ApplicationHelper
 
   def level_class_selector(params, level)
-    page_level = Level.find_by_slug(params[:id])
+    if params[:lesson_id]
+      lesson = Lesson.find_by_slug(params[:lesson_id])
+      page_level = lesson.levels.find_by_slug(params[:id])
+    end
     if page_level == level
       "active"
     elsif page_level && page_level.sequence_number > level.sequence_number
