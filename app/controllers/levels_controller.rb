4bd55cbbbad139b4d19c546a3900ab6e6def756f
Hashrocket Workstation <dev@hashrocket.com>
2013-07-02 11:30:50 -0400

    Remove ununsed variables

--- app/controllers/levels_controller.rb
+++ app/controllers/levels_controller.rb
@@ -4,10 +4,7 @@ class LevelsController < ApplicationController
   expose(:level, attributes: :level_params, finder: :find_by_slug)
   expose(:commands)   { level.commands }
   expose(:command)      { commands.first }
-  expose(:next_command) { commands.second }
-  expose(:current_cycle) { 1 }
   expose(:next_level) { level.next_level }
-  expose(:question_number) { 1 }
 
   def show
     commands = current_user.commands_remaining_for_level(level)
