c952c160ed2e22fae2c2f08bb7303fbf3e5448cd
Hashrocket Workstation <dev@hashrocket.com>
2013-07-03 09:47:08 -0400

    Update mousetrap

--- app/assets/javascripts/mousetrap.js
+++ app/assets/javascripts/mousetrap.js
@@ -17,7 +17,7 @@
  * Mousetrap is a simple keyboard shortcut library for Javascript with
  * no external dependencies
  *
- * @version 1.4.2
+ * @version 1.4.3
  * @url craig.is/killing/mice
  */
 (function() {
@@ -175,6 +175,13 @@
         _ignoreNextKeyup = false,
 
         /**
+         * temporary state where we will ignore the next keypress
+         *
+         * @type {boolean}
+         */
+        _ignoreNextKeypress = false,
+
+        /**
          * are we currently inside of a sequence?
          * type of action ("keyup" or "keydown" or "keypress") or false
          *
@@ -237,6 +244,12 @@
             // trigger the event.  shift+a will though.
             if (!e.shiftKey) {
                 character = character.toLowerCase();
+            }
+
+            // String.fromCharCode(32) for spacebar returns " " for the
+            // character name, make sure it matches the mousetrap name
+            if (character == ' ') {
+                return 'space';
             }
 
             return character;
@@ -496,9 +509,22 @@
         // modifier keys are ignored because you can have a sequence
         // that contains modifiers such as "enter ctrl+space" and in most
         // cases the modifier key will be pressed before the next key
-        if (e.type == _nextExpectedAction && !_isModifier(character)) {
+        //
+        // also if you have a sequence such as "ctrl+b a" then pressing the
+        // "b" key will trigger a "keypress" and a "keydown"
+        //
+        // the "keydown" is expected when there is a modifier, but the
+        // "keypress" ends up matching the _nextExpectedAction since it occurs
+        // after and that causes the sequence to reset
+        //
+        // we ignore keypresses in a sequence that directly follow a keydown
+        // for the same character
+        var ignoreThisKeypress = e.type == 'keypress' && character === _ignoreNextKeypress;
+        if (e.type == _nextExpectedAction && !_isModifier(character) && !ignoreThisKeypress) {
             _resetSequences(doNotReset);
         }
+
+        _ignoreNextKeypress = processedSequenceCallback && e.type == 'keydown' ? character : false;
     }
 
     /**
@@ -551,7 +577,7 @@
      */
     function _resetSequenceTimer() {
         clearTimeout(_resetTimer);
-        _resetTimer = setTimeout(_resetSequences, 10000);
+        _resetTimer = setTimeout(_resetSequences, 1000);
     }
 
     /**
