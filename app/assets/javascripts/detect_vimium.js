bce43b8648e120ecd12053cdd6fbea7ed16dc08b
Hashrocket Workstation <dev@hashrocket.com>
2013-07-03 16:16:01 -0400

    Add vimium warning

---
+++ app/assets/javascripts/detect_vimium.js
@@ -0,0 +1,36 @@
+// Detect the Vimium Chrome extension by Evan Hahn (evanhahn.com)
+
+
+;(function(doc) {
+
+  doc.hasVimium = function() {
+    return doc.getElementsByClassName('vimiumReset').length > 0;
+  };
+
+  doc.onDetectVimium = function(callback, scope) {
+
+    scope || (scope = window);
+
+    if (doc.hasVimium()) {
+      callback.call(scope);
+    }
+
+    else {
+      var interval = setInterval(function() {
+        if (doc.hasVimium()) {
+          clearInterval(interval);
+          callback.call(scope);
+        }
+      }, 1000);
+    }
+
+  };
+
+})(document);
+
+
+
+document.onDetectVimium(function() {
+  $('#vimium_warning').show()
+  $('#vimium_warning .error').show()
+});
