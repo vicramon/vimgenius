d162c89b172131e32c79c31afac7ffd5834e11b1
Vic Ramon <vic@vicramon.com>
2014-07-07 15:05:09 -0400

    Make admin more secure

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,16 +2,26 @@ class AdminController < ApplicationController
   before_filter :authenticate_admin, except: :login
 
   def authenticate_admin
-    redirect_to admin_login_path unless cookies[:admin] == ENV.fetch("ADMIN_PASSWORD")
+    redirect_to admin_login_path unless cookies[:admin] == hashed_admin_password
   end
 
   def login
     if request.post?
-      if params[:password] == "vimmaster"
-        cookies[:admin] = "vimmaster"
+      if params[:password] == admin_password
+        cookies[:admin] = hashed_admin_password
         redirect_to admin_root_path and return
       end
     end
+  end
+
+  private
+
+  def admin_password
+    ENV.fetch("ADMIN_PASSWORD")
+  end
+
+  def hashed_admin_password
+    Digest::SHA1.hexdigest admin_password
   end
 
 end
